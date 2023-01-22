4cb1b6128cae9cc7bba9ad81cc94a94741f46d18
Perc <carlpericles18@gmail.com>
2023-01-22 15:03:25 +0900

    Fixing Email API

--- js/contact.js
+++ js/contact.js
@@ -7,17 +7,11 @@ const articleText = document.querySelector('.article-Text');
 const body = document.querySelector('.body')
 const active = document.querySelector('.active')
 const mainLogo = document.querySelector(".mainLogo")
-
-
-
-
+const submit = document.querySelector(".sub")
 
 function homepage(){
     window.location.href='/index.html'
 }
-
-
-
 
 
     mobilePov.addEventListener('click',()=>{   
@@ -30,11 +24,36 @@ function homepage(){
 
 
 
-// ARTICLES PUSH
 
 
-// window.addEventListener('load',()=>{
-// })
+//SENDING EMAIL CODE
+function sendMail(){
+    let params ={
+        from_name : document.getElementById("name").value,
+        email_id: document.getElementById("email").value,
+        subject : document.getElementById("subject").value,
+        message : document.getElementById("message").value
+    };
+const serviceId = "service_qxgsdwm"
+const templateId = "template_v2moqep"
+
+emailjs.send(serviceId, templateId,params)
+.then(
+    res =>{
+        document.getElementById("name").value = "";
+        document.getElementById("email").value = "";
+        document.getElementById("subject").value = "";
+        document.getElementById("message").value = "";
+        console.log(res)
+        alert('MESSAGE SEND')
+    })
+    .catch((err)=>{
+        console.log(err)
+    })
+};
+
+
+
 
 
 
